0b8ee4baebb6644e1f2e9073ce3d81c90b2481b7
Omar Naman <onaman@uwaterloo.ca>
2021-06-23 06:15:15 -0400

    fix bug opening proxy url

--- background.js
+++ background.js
@@ -7,8 +7,9 @@ function open_link() {
         if (tabs[0]) {
             let currentTab = tabs[0];
             let url = currentTab.url;
-            var new_tab = browser.tabs.create({ url: url })
-            return `https://proxy.lib.uwaterloo.ca/login?url=${url}`;
+            let proxy_url = `https://proxy.lib.uwaterloo.ca/login?url=${url}`
+            var new_tab = browser.tabs.create({ url: proxy_url })
+
         }
     }
     var gettingActiveTab = browser.tabs.query({ active: true, currentWindow: true });
